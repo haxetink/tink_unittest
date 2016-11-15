427745d3ad715f5d6f96abe67a5bd32014f6d163
kevinresol <kevinresol@gmail.com>
2016-11-15 17:48:38 +0800

    Add multi assertion example

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -32,7 +32,7 @@ class NormalTest {
 	public function asyncAssert()
 		return Future.sync(isTrue(true));
 		
-	@:timeout(500) // in ms
+	@:timeout(1500) // in ms
 	@:describe('Timeout test')
 	public function timeout()
 		return Future.async(function(cb) haxe.Timer.delay(function() cb(isTrue(true)), 1000));
@@ -43,6 +43,10 @@ class NormalTest {
 	@:describe('    descriptions')
 	public function nestedDescriptions()
 		return isTrue(true);
+    
+	@:describe('Multiple assertions')
+	public function multiAssert()
+		return isTrue(true) && isTrue(true) && isTrue(true);
 }
 
 @:await
@@ -52,7 +56,7 @@ class AwaitTest {
 	@:describe('Async test powered by tink_await')
 	@:async public function async() {
 		var actual = @:await someAsyncValue();
-		return equals('expected', actual);
+		return equals('actual', actual);
 	}
   
   function someAsyncValue() 
