63947b3c910610541c980f35a6b9611d2abd8903
Kevin Leung <kevinresol@gmail.com>
2017-03-18 11:59:55 +0800

    Fix cs test

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -13,7 +13,12 @@ class RunTests {
 		
 		var code = 0;
 		
+		
+		#if cs // https://github.com/HaxeFoundation/haxe/issues/6106
+		function assertEquals(expected:Dynamic, actual:Dynamic, ?pos:haxe.PosInfos) {
+		#else
 		function assertEquals<T>(expected:T, actual:T, ?pos:haxe.PosInfos) {
+		#end
 			if(expected != actual) {
 				println('${pos.fileName}:${pos.lineNumber}: Expected $expected but got $actual ');
 				code++;
