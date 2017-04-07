db28d2c72ebdb1dac4258976227f3d9d002fa73f
Kevin Leung <kevinresol@gmail.com>
2017-04-07 23:26:19 +0800

    Use Expr

--- src/tink/unit/TestSuite.hx
+++ src/tink/unit/TestSuite.hx
@@ -13,8 +13,8 @@ class TestSuiteBase<T> extends BasicSuite {
 #end
 
 class TestSuite {
-	public static macro function make(e:haxe.macro.Expr, ?name:String) {
+	public static macro function make(e:haxe.macro.Expr, ?name:haxe.macro.Expr) {
 		var ct = haxe.macro.Context.toComplexType(haxe.macro.Context.typeof(e));
-		return macro new tink.unit.TestSuite.TestSuiteBuilder<$ct>($e, $v{name});
+		return macro new tink.unit.TestSuite.TestSuiteBuilder<$ct>($e, $name);
 	}
 }
