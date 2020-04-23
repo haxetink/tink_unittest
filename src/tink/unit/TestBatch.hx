e88272ed3cfb8f761fd4d317bc84147e357a4b13
Juraj Kirchheim <back2dos@googlemail.com>
2020-04-24 00:21:48 +0200

    Resolves #18.

--- src/tink/unit/TestBatch.hx
+++ src/tink/unit/TestBatch.hx
@@ -1,18 +1,20 @@
 package tink.unit;
 
 #if macro
+import haxe.macro.Context;
 import haxe.macro.Expr;
 using tink.MacroApi;
 #end
 
 class TestBatch {
-	public static macro function make(e:Expr) {
-		return switch e.expr {
-			case EArrayDecl(values):
-				var suites = EArrayDecl(values.map(function(v) return macro tink.unit.TestSuite.make($v))).at(e.pos);
-				macro new tink.testrunner.Batch($suites);
+	public static macro function make(values:Array<Expr>) {
+		switch values {
+			case [{ expr: EArrayDecl(exprs) }]:
+				values = exprs;
 			default:
-				e.pos.error('Expected Array');
 		}
+
+		var suites = EArrayDecl(values.map(function(v) return macro tink.unit.TestSuite.make($v))).at();
+		return macro new tink.testrunner.Batch($suites);
 	}
 }
